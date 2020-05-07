679ba3137a31ea6a3a9414113f057cb031353c1e
marcelormartins2 <marcelormartins2@gmail.com>
2020-05-07 01:27:12 -0300

    Migration Inicial

--- InvestCarControl/Startup.cs
+++ InvestCarControl/Startup.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using InvestCarControl.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -34,6 +36,9 @@ namespace InvestCarControl
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddDbContext<MyDbContext>(options =>
+                    options.UseMySql(Configuration.GetConnectionString("MyDbContext"), builder =>
+                        builder.MigrationsAssembly("InvestCarControl")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
